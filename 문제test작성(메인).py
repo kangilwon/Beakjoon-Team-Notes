b8e3bb452dd1596993b68cf6a5bf5711a68d067d
kangilwon <eleck11@naver.com>
2021-12-13 22:28:29 +0900

    commit

--- 문제test작성(메인).py
+++ 문제test작성(메인).py
@@ -1,18 +1,3 @@
-a=int(input())
-b=int(input())
-z=[]
-for num in range(a,b+1):
-    error=0
-    if num > 1:
-        for i in range(2, num):
-            if num%i==0:
-                error+=1
-                break
-        if error==0:
-            z.append(num)
-
-if len(z)>0 :
-    print(sum(z))
-    print(min(z))
-else:
-    print('-1')
+for i in range(int(input())):
+    a,b=map(int,input().split(','))
+    print(str(a+b))
