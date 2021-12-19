aba0e9169307f245f4bd81ba79ede91df3cabb28
kangilwon <eleck11@naver.com>
2021-12-19 16:04:13 +0900

    20211219

--- 문제test작성(서브).py
+++ 문제test작성(서브).py
@@ -1,14 +1,7 @@
-t = input()
-a=list(map(int,input().split()))
-a.sort()
+a = int(input())
+b = int(input())
+c = int(input())
 
-grop=0
-cnt=0
-
-for i in a:
-    cnt+=1
-    if cnt >=i:
-        grop+=1
-        cnt=0
-print(grop)
-    
+result = list(str(a * b * c))
+for i in range(10):
+    print(result.count(str(i)))
