e432f6b059e07f7c60e412aa10b35226908428c7
kangilwon <eleck11@naver.com>
2022-01-07 13:49:40 +0900

    20220107

--- python/문제풀이/프로그래머스 문제풀이/1단계/신규 아이디 추천.py
+++ python/문제풀이/프로그래머스 문제풀이/1단계/신규 아이디 추천.py
@@ -1,28 +1,41 @@
 def solution(new_id):
-    # answer=list(answer)
-    # 문자제약(알파벳 소문자, 숫자, . - _)
-    as_num=48 # 아스키코드 숫자 0
-    str_o=list(range(97,123)) # 아스키코드 알파벳 소문자 a~z
-    str_o.append(45) # 아스키코드 - -
-    str_o.append(46) # 아스키코드 - .
-    str_o.append(95) # 아스키코드 - _
+    answer=''
 
-    # int 형태의 리스트의 value를 아스키 코드로 변환한다.
-    for i in range(len(str_o)):
-        str_o[i]=chr(str_o[i])
-    # 숫자의 아스키코드 0 ~ 9 까지
-    for i in range(10):
-        str_o.append(chr(as_num))
-        as_num+=1
-        
-    answer = str_o
+    # 1단계 모든 대문자를 대응되는 소문자로 치환합니다.
+    new_id=new_id.lower()
 
-    # for i in (list(answer)):
-    #     if i not in str_o:
-    #         answer=answer-i
-    
+    # 2단계 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+    for i in new_id:
+        if i.isalnum() or i in ['-', '_', '.']:
+            answer += i
+
+    # 3단계 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+
+    # 4단계 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+    if answer[0] == '.':
+        answer = answer[1:] if len(answer) > 1 else '.'
+
+    if answer[-1] == '.':
+        answer = answer[:-1]
+
+    # 5단계 빈 문자열이라면, answer에 "a"를 대입합니다.
+    if answer == '':
+        answer = 'a'
+
+    # 6단계 길이가 16자 이상이면, answer의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+        #만약 제거 후 마침표(.)가 answer의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+    if len(answer) > 15:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+
+    # 7단계 길이가 2자 이하라면, answer의 마지막 문자를 answer의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+    while len(answer) < 3:
+        answer += answer[-1]
+
 
     return answer
     
-id=input().lower
-print(solution(id))
+solution(input())
