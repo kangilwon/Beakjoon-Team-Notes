705e991fc2b067b35b3638b762147e6951eaedb7
kangilwon <eleck11@naver.com>
2022-01-11 16:41:45 +0900

    추가

--- 문제test작성(메인).py
+++ 문제test작성(메인).py
@@ -1,7 +1,6 @@
-t=int(input())
-
-p_list=[0,1,1]
-for i in range(2,t):
-    a=p_list[i]+p_list[i-1]
-    p_list.append(a)
-print(p_list[t])
+d=''
+c=list(map(int,input().split()))
+c.sort()
+for i in range(len(c)):
+    d+=str(c[i])+' '
+print(d[:-1])    
