642cfec0eeb3f28a7574045f04bbd5a82c8da8f7
kangilwon <eleck11@naver.com>
2022-01-24 14:20:33 +0900

    20220124_2

--- 문제test작성(메인).py
+++ 문제test작성(메인).py
@@ -1,16 +1,1 @@
-t = int(input())
-
-for i in range(t):
-    a, b, c = map(int, input().split())
-    cnt=0
-    while True:
-        if c <= 0:
-            c+=a
-            break
-        cnt+=1
-        c-=a
-    c *= 100 
-    
-    
-    print(c+cnt)
-    
+print(bin(int(str(int(input())),8))[2:])
