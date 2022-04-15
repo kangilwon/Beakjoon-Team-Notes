575c3cdcf8b2435e0f3973e4ae87d20a6b65f492
kangilwon <eleck11@naver.com>
2022-04-15 12:44:48 +0900

    .

--- python/문제풀이/프로그래머스 문제풀이/2단계/더맵게(힙).py
+++ python/문제풀이/프로그래머스 문제풀이/2단계/더맵게(힙).py
@@ -1,20 +1,16 @@
-def solution(scoville, K):
-    answer = []
-    scoville.sort()
-    x=scoville.pop(0)
-    while True:
-        scoville.sort()
-        if x >= K:
-            answer.append(x)
-            x=scoville.pop(0)
-        else:
-            a=min(x,scoville[0])
-            b=max(x,scoville[0])
-            x=a+(b*2)
-            scoville.pop(0)
-        if len(scoville)==0:
-            break
-        print(answer,x)
-    return answer
+import heapq
 
-print(solution([1, 2, 3, 9, 10, 12], 7))
+
+def solution(scoville, k):
+    hot = []
+    for i in scoville:
+        heapq.heappush(hot, i)
+
+    cnt = 0
+    while hot[0] < k:
+        try:
+            heapq.heappush(hot, heapq.heappop(hot) + (heapq.heappop(hot) * 2))
+        except IndexError:
+            return -1
+        cnt += 1
+    return cnt
